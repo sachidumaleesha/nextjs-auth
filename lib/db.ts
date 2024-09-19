8c3b8cc4e3d08430aa139fb985c226c2627ed4ba
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-09-19 16:25:16 +0530

    fix eslint error

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
